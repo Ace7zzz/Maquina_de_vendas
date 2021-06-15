6bc1230975035eb1f548da4a1e029214767da521
amandassa <amandassa504@gmail.com>
2021-06-15 15:18:36 -0300

    decodificador do teclado

---
+++ teclado.v
@@ -0,0 +1,23 @@
+module teclado (
+input [3:0] in,
+output reg [3:0] out
+);
+
+always @ (in) begin
+	if (in[0] != 0) begin
+	out <= 3'b111;
+	end
+	else if (in[1] != 0) begin
+	out <= 3'b001;
+	end
+	else if (in[2] != 0) begin
+	out <= 3'b010;
+	end
+	else if (in[3] != 0) begin
+	out <= 3'b011;
+	end
+	else begin
+	out <= 2'b000;
+	end
+end
+endmodule 
