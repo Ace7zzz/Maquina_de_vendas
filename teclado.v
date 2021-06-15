8ea5edafcba5dcb52f924ffd20a70ba6b58fc78a
amandassa <amandassa504@gmail.com>
2021-06-15 15:29:11 -0300

    teclado 2 bits

--- teclado.v
+++ teclado.v
@@ -4,17 +4,18 @@ output reg [2:0] out
 );
 
 always @ (in) begin
-	if (in[0] != 0) begin
-	out <= 2'b00;
-	end
-	else if (in[1] != 0) begin
+	if (in[1] != 0) begin
 	out <= 2'b01;
 	end
 	else if (in[2] != 0) begin
 	out <= 2'b10;
 	end
-	else begin
+	else if begin
 	out <= 2'b11;
 	end
+	else begin
+	out <= 2'b00;
+	end
+
 end
 endmodule 
